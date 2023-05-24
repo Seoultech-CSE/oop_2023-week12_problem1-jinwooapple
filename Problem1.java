eae3f38e583bc060f26d5d5bb96e76f5acf9fc2d
jinwooapple <131870368+jinwooapple@users.noreply.github.com>
2023-05-24 19:54:17 +0900

    Update Problem1.java
    주석 처리된 부분을 수정하였다.

--- Problem1.java
+++ Problem1.java
@@ -1,29 +1,55 @@
 import java.util.*;
 
 public class week13_1 {
+     static Pair[] arr=new Pair[10];
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // 필요한 자료형을 넣어주세요
-        ArrayList< __________ > list = new ArrayList< __________ >();
+       ArrayList< String > list = new ArrayList<String>();
         System.out.print("Enter five integers and five doubles: ");
 
-        /*
-            list 입력 코드
-         */
-
+        
+        for(int i=0;i<10;++i)
+        {
+            list.add(input.next());
+        }
+        
         sort(list);
-
-        /*
-            list 출력 코드
-         */
+        
+        for(int i=0;i<10;++i)
+        {
+            System.out.print(list.get(arr[i].index)+" ");
+        }
+        input.close();
     }
 
-    public static void sort( __________ ) {
+   public static void sort(ArrayList<String> list) 
+    {
+        
+        for(int i=0;i<10;++i)
+        {
+            arr[i]=new Pair(Double.parseDouble(list.get(i)),i); 
+        }
+        Arrays.sort(arr);
 
-         /*
-            list 정렬 코드
-         */
+        
+    }
+    static class Pair implements Comparable<Pair>
+    {
+        public double d;
+        public int index;
+        Pair(double dd, int idx )
+        {
+            d=dd;index=idx;
+        }
 
+        @Override
+        public int compareTo(Pair p)
+        {
+            if(Double.compare(this.d,p.d)<0) return -1;
+            else if(Double.compare(this.d,p.d)==0) return 0;
+            else return 1;
+
+        }
     }
 }
